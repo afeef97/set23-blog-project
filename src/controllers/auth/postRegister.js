4eed851aee01f12bc9899ac7c31ebe71b14fc2b0
Afeef <afeef9706@gmail.com>
2023-08-06 22:51:06 +0800

    Removed erroneous import in postRegister

--- src/controllers/auth/postRegister.js
+++ src/controllers/auth/postRegister.js
@@ -1,5 +1,4 @@
 import queryDB from "../../database/dbBlog";
-import hashPassword from "./hashPassword";
 import bcrypt from "bcryptjs";
 
 export async function registerUser(req, res) {
@@ -10,11 +9,13 @@ export async function registerUser(req, res) {
     await queryDB(
         "INSERT INTO blog_users (user_email, username, user_password, first_name, last_name, is_admin) VALUES ($1, $2, $3, $4, $5, $6)",
         [email, username, hashedPassword, f_name, l_name, isAdmin]
-    ).then(function(resDb) {
-        res.status(200).json({ message: "New user created" })
-    }).catch(function (errDb) {
-        res.status(500).json({ message: "Server error", errDb })
-    })
+    )
+        .then(function (resDb) {
+            res.status(200).json({ message: "New user created" });
+        })
+        .catch(function (errDb) {
+            res.status(500).json({ message: "Server error", errDb });
+        });
 }
 
 function hashPassword(password) {
